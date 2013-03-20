446eb8e1adf996b85f8f1ef2f79168363e1405cb
Razvan Vuscan <razvan.vuscan@xoomworks.com>
2013-03-20 10:42:18 +0300

    * Added some changes...

--- Demo/src/test/java/Tests/NewTest.java
+++ Demo/src/test/java/Tests/NewTest.java
@@ -1,9 +1,12 @@
 package Tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class NewTest {
   @Test
   public void f() {
+	  WebDriver driver = new FirefoxDriver();
   }
 }
